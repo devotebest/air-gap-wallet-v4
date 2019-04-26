503641e1f69777bf32bfd30a8200ef53788b4a72
devotebest <bestmob310@gmail.com>
2019-04-26 23:24:28 +0400

    fix prepare transaction

--- src/app/pages/portfolio/portfolio.module.ts
+++ src/app/pages/portfolio/portfolio.module.ts
@@ -2,7 +2,7 @@ import { IonicModule } from '@ionic/angular'
 import { RouterModule } from '@angular/router'
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { FormsModule } from '@angular/forms'
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 import { PortfolioPage } from './portfolio'
 import { ComponentsModule } from '../../components/components.module'
 import { TranslateModule } from '@ngx-translate/core'
@@ -12,6 +12,7 @@ import { TranslateModule } from '@ngx-translate/core'
     IonicModule,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     ComponentsModule,
     TranslateModule,
     RouterModule.forChild([{ path: '', component: PortfolioPage }])
